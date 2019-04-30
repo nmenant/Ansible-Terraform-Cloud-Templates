d750e99393e8fcf737e914accd93568c86e74f7a
Nicolas Ménant <menant.nicolas@gmail.com>
2019-04-29 18:42:02 -0700

    fixed typo

--- terraform-ansible-azure/F5_Standalone_1Nic/terraform/init-azure-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Setting environment variables for Terraform"
-export ARM_SUBSCRIPTION_ID=aacd7ba7-e47c-4cb7-a8b7-90f81fdd3865
-export ARM_CLIENT_ID=e07fa29d-7a92-461c-ac0e-7306ce16f6f1
-export ARM_CLIENT_SECRET=bZJGHCxlPhOvuAbaAgQlU77WUxmLeDPBLtCCvPS+qtI=
-export ARM_TENANT_ID=e569f29e-b098-4cea-b6f0-48fa8532d64a
-
